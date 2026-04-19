025144d0440fa84e9231364d81fa9812ad9a66d1
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2026-04-19 17:39:29 -0400

    update

--- codes-clases/20260417-01/example.cc
+++ codes-clases/20260417-01/example.cc
@@ -19,6 +19,11 @@ void f02(double a, double b, double* resultado){
     *resultado = a + b;
 }
 
+/*
+Esta es una prueba de funcionamiento para:
+   1) paso de argumentos por valor.
+   2) conversión implícita de tipo de tipos.
+*/
 void prueba01(){
     int a, b;
     float resultado;
@@ -35,19 +40,47 @@ void prueba01(){
     printf("main: b = %i\n", b);
     printf("resultado = %f\n", resultado);
 }
-
+/*
+    Funcionamiento básico de punteros
+*/
 void prueba02(){
     int a = 10;
+
+    // Mostrar la dirección de memoria de la variable a
     printf("&a = %p\n", &a);
 
+    // guardar la dirección de memoria de una variable.
+    // Para esto, se necesita un tipo especial de variable
+    // que se llama puntero. La sintaxis de declaración
+    // para estas variables es que el símbolo '*' está al lado
+    // izquierdo del nombre de la variable.
+    // Además, para acceder al dato que es referenciado por 
+    // dicha dirección, se debe agrega el tipo de contenido
+    // que guarda dicha dirección.
+
+    // Pr ejemplo, la dirección de la variable 'a' se guarda en el
+    // puntero 'ptr_a'. Además, se informa al lenguaje que el dato 
+    // de la variable 'a' es de tipo entero.
     int* ptr_a = &a;
+
+    // Se muestra el contenido de la variable 'ptr_a':
     printf("ptr_a  = %p\n", ptr_a);
+
+    // Con el operador '*', se puede acceder al dato que es referenciado
+    // por la dirección guardada en el puntero 'ptr_a':
     printf("*ptr_a = %i\n", *ptr_a);
 
+    // También se puede modificar el contenido de la dirección de memoria
+    // almacenada en el puntero:
     *ptr_a = 51;
     printf("a      = %i\n", a);
     printf("*ptr_a = %i\n", *ptr_a);
+}
 
+/*
+    Prueba de concepto de paso de referencia 
+*/
+void prueba03(){
     int a0 = 20;
     int b0 = 30;
     double res;
@@ -61,7 +94,8 @@ void prueba02(){
 int main()
 {
     //prueba01();
-    prueba02();
+    //prueba02();
+    prueba03();
 
     exit(EXIT_SUCCESS);
 }
