4f5a304ea175ad9c1f4426c3e6f1f76749b7f076
Gabriel <gabriel@Tatooine.local>
2026-05-06 23:22:44 -0400

    arduino examples

---
+++ codes-examples/arduino-codes/test01/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+uint8_t ledPin = 7;
+
+// blink example
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(ledPin, LOW);  // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+}
